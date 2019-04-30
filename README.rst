96821599b5823e48e3c5022d4673a59742eb59ab
John Thomson <github@auto.jwdt.co.uk>
2019-04-30 23:21:54 +0100

    j_html_pdf-1 This should be available via pip install. Added settings to allow publishing to pypi: https://blog.jetbrains.com/pycharm/2017/05/how-to-publish-your-package-on-pypi/

---
+++ README.rst
@@ -0,0 +1,1 @@
+This is basically just a wrapper around Weasyprint.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='j_html_pdf',
+    version='1.0.0',
+    packages=['j_html_pdf'],
+    url='https://github.com/JWDT/j_html_pdf',
+    license='The Unlicense',
+    author='jdev',
+    author_email='pypi@johken.uk',
+    description='Wrapper around weasyprint.'
+)
